6bad4740d903a1c5a22b0fbf10e16de847837dbe
Lukas <lg2711@hotmail.de>
2023-11-23 11:06:04 +0100

    💄 Add lifecycle SVG file

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <img class="img-fluid" :src="lifecycle" />
 </template>
+
+<script setup>
+import lifecycle from "/static/img/ML_lifecycle.svg";
+</script>
+
+<style scoped>
+</style>
