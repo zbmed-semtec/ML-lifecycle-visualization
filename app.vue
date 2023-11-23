a2c3fd44869228bdd1e4afa9aa6d9bfc186c9968
rohitharavinder <rohitha0112@gmail.com>
2023-11-23 14:33:23 +0100

    add function to parse TSV

--- app.vue
+++ app.vue
@@ -1,12 +1,41 @@
 <template>
   <StepNode />
   <lifecycle />
+  <input type="file" @change="handleTxtFileUpload" accept=".tsv" />
 </template>
 
 <script setup>
 import StepNode from "~/components/StepNode.vue";
 import lifecycle from "../static/img/ML_lifecycle.svg";
+
+//Library for parsing the .csv file
+import Papa from "papaparse";
+
+
+const handleTxtFileUpload = (event) => {
+  const reader = new FileReader();
+  reader.onload = () => {
+    const textFile = reader.result;
+    const rows = Papa.parse(textFile.trim(), {
+      header: true,
+      skipEmptyLines: true,
+    }).data;
+    // console.log(rows)
+    const textContent = rows.map(row => {
+      return {
+        node_id: row.NodeID,
+        name: row.Name,
+        description: row.Description,
+        outcome: row.Outcome,
+        notes: row.Notes,
+      };
+  });
+  }
+  reader.readAsText(event.target.files[0]);
+};
+
 </script>
 
 <style scoped>
 </style>
+
