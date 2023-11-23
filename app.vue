3efad6e436a7a13512d96e0db10a181a82903942
Aurélien Luciani <aurel-l@users.noreply.github.com>
2023-11-23 15:11:29 +0100

    add manual step selector

--- app.vue
+++ app.vue
@@ -1,22 +1,22 @@
 <template>
+  <Selector v-model:value="selectedStep" />
+  <p>Selected step: {{ selectedStep }}</p>
   <lifecycle />
   <Table :data="table" />
   <input type="file" @change="handleTxtFileUpload" accept=".tsv" />
 </template>
 
 <script setup>
+//Library for parsing the .csv file
+import Papa from "papaparse";
+
+import Selector from "~/components/Selector.vue";
 import MetadataModal from "~/components/MetadataModal.vue";
 import StepNode from "~/components/StepNode.vue";
 import lifecycle from "../static/img/ML_lifecycle.svg";
 import Table from "~/components/Table.vue";
 
 const table = [[1,2,3,4,5,6,7,8],['a','b','c','d','e','f','g','h']];
-</script>
-
-
-//Library for parsing the .csv file
-import Papa from "papaparse";
-
 
 const handleTxtFileUpload = (event) => {
   const reader = new FileReader();
@@ -40,35 +40,13 @@ const handleTxtFileUpload = (event) => {
   reader.readAsText(event.target.files[0]);
 };
 
+const selectedStep = ref(2)
+
 </script>
 
 <style scoped>
 html {
   font-family: "helvetica neue", helvetica, arial, sans-serif;
-}
-
-table ,th, td{
-  border: 1px solid;
-  font-size: 15px;
-  width: 80;
-  border-collapse: collapse;
-  
-}
-
-th
-    {
-      background-color:lightblue;
-      color:black;
-    }
-
-td {
-  text-align: center;
-  font-size: 15px
-}
-tr
-{
-  background-color:lightgray;
-  color:black;
 }
 </style>
 
---
+++ components/Selector.vue
@@ -0,0 +1,24 @@
+<template>
+   <div>
+      <label>
+         Select a step:
+         <select
+            :value="value"
+            @change="$emit('update:value', $event.target.value)"
+         >
+            <option v-for="option of options" :value="option">
+               {{ option }}
+            </option>
+         </select>
+      </label>
+   </div>
+</template>
+
+<script setup>
+defineProps(['value'])
+defineEmits(['update:value'])
+const options = Array.from({length: 10}, (_, index) => index);
+</script>
+
+<style scoped>
+</style>
