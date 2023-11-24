dd88f8e2719938e336fb79682152eaa441390ce9
Dhwanisolanki12 <dhwanisolanki128@gmail.com>
2023-11-24 09:55:22 +0100

    linking the text

---
+++ components/Cell.vue
@@ -0,0 +1,23 @@
+<template>
+   <tr v-for="row of data">
+      <Cell :data="" />
+      <td>{{row["NodeID"]}}</td>
+      <td>{{row["FAIR Principles"]}}</td>
+      <td>{{row["Best Practices"]}}</td>
+      <td>
+        <a v-if="row['Metadata schemas'].startsWith('http')" :href="row['Metadata schemas']">{{ row["Metadata schemas"] }}</a>
+        <span v-else>{{row["Metadata schemas"]}}</span>
+      </td>
+      <td>{{row["Services"]}}</td>
+      <td>{{row["What do you need to do here"] }}</td>
+      <td>{{row["Resources affected by FAIR"] }}</td>
+      <td>{{row["Notes"]}}</td>
+    </tr>
+   
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
--- components/Table.vue
+++ components/Table.vue
@@ -18,10 +18,15 @@
   </thead>
   <tbody>
     <tr v-for="row of data">
+       <Cell :data="" />
       <td>{{row["NodeID"]}}</td>
       <td>{{row["FAIR Principles"]}}</td>
       <td>{{row["Best Practices"]}}</td>
       <td>{{row["Metadata schemas"]}}</td>
+        <td>
+        <a v-if="row['Metadata schemas'].startsWith('http')" :href="row['Metadata schemas']">{{ row["Metadata schemas"] }}</a>
+        <span v-else>{{row["Metadata schemas"]}}</span>
+      </td>
       <td>{{row["Services"]}}</td>
       <td>{{row["What do you need to do here"] }}</td>
       <td>{{row["Resources affected by FAIR"] }}</td>
