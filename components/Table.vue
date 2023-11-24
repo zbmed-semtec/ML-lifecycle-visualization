d8b1d3c65a9c59396cefe2609418a06c7f9e2432
rohitharavinder <rohitha0112@gmail.com>
2023-11-24 11:58:28 +0100

    change cursor pointer and highlight the selected step

--- components/Table.vue
+++ components/Table.vue
@@ -24,6 +24,53 @@
         <Cell :cellData="row['Notes']" />
       </tr>
   </tbody>
+    <table border="1">
+        
+  <!-- <caption>
+    ML Life Cycle Visualization <br>
+  </caption> -->
+  <thead>
+  <tr>
+    <th>Title</th>
+    <th>FAIR Principle</th>
+    <th>Best Practices</th>
+    <th>Metadata schemas</th>
+    <th>Services</th>
+    <th>Best Practices</th>
+    <th>Resources affected by FAIR</th>
+    <th>Notes</th>
+  </tr>
+  </thead>
+  <tbody>
+    <tr v-for="row of data">
+       <!-- <Cell :data="" /> -->
+      <td>{{row["NodeID"]}}</td>
+      <td>{{row["FAIR Principles"]}}</td>
+      <td>{{row["Best Practices"]}}</td>
+      <td>{{row["Metadata schemas"]}}</td>
+        <td>
+        <a v-if="row['Metadata schemas'].startsWith('http')" :href="row['Metadata schemas']">{{ row["Metadata schemas"] }}</a>
+        <span v-else>{{row["Metadata schemas"]}}</span>
+      </td>
+      <td>{{row["Services"]}}</td>
+      <td>{{row["What do you need to do here"] }}</td>
+      <td>{{row["Resources affected by FAIR"] }}</td>
+      <td>{{row["Notes"]}}</td>
+    </tr>
+
+  <!-- <tr>
+    <th colspan="2">Problem Definition</th>
+        <td><tr>FAIR Data</tr>
+        <tr>FAIR Software</tr>
+        <tr>FAIR AI Models</tr>
+        </td>   
+  </tr>
+  <tr>
+    <th scope="row">Khiresh Odo</th>
+    <td>7</td>
+    <td>7,223</td>
+  </tr> -->
+</tbody>
 </table>
 </template>
  
