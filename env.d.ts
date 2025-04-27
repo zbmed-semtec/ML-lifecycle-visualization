5c27728719c717ab856a1a0f36b4ea65672a79a0
bit2424 <nelsonquinones2424@gmail.com>
2025-04-27 18:52:39 +0000

    :sparkles: Extend TypeScript declarations to support Vue and SVG components

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+declare module '*.vue' {
+  import type { ComponentOptions } from 'vue'
+  const component: ComponentOptions
+  export default component
+}
+
+declare module '*.svg' {
+  import type { Component } from 'vue'
+  const component: Component
+  export default component
+}
